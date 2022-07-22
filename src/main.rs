1efaa0ae6552a5d906b8fadad8d04410fa39f0c9
cbacary <cbacary4@gmail.com>
2022-07-22 18:02:05 -0400

    Fix calculate_move

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use chess::{Board, MoveGen, ChessMove, Square, Color, Piece, BoardStatus};
 use std::cmp;
 
 struct ReturnValue {
-    best_move: ChessMove,
+    best_move: Option<ChessMove>,
     position_evaluation: i32
 }
 
@@ -65,44 +65,43 @@ fn calculate_position(board: &Board) -> i32 {
 
 // In some cases we need to return the value of the node
 
-fn calculate_move(mut board: &Board, depth: u8, original_depth: u8, player: bool, mut alpha: i32, mut beta: i32) -> ReturnValue {
+fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, optimizing_color: Color, mut alpha: i32, mut beta: i32) -> ReturnValue {
     let status = board.status();
-    if depth == 0 || board.status() != BoardStatus::Ongoing {
-        if player {
+    if depth == 0 || status != BoardStatus::Ongoing {
+        if optimizing_color == Color::White {
             match status {
                 BoardStatus::Ongoing => return ReturnValue  { 
-                    best_move: ChessMove::new(Square::A1, Square::A2, Option::None),
+                    best_move: None,
                     position_evaluation: calculate_position(board)
                 },
                 BoardStatus::Checkmate => return ReturnValue  { 
-                    best_move: ChessMove::new(Square::A1, Square::A2, Option::None),
-                    position_evaluation: -999999
+                    best_move: None,
+                    position_evaluation: if player {999999} else {-999999}
                 },
                 BoardStatus::Stalemate => { return ReturnValue {
-                    best_move: ChessMove::new(Square::A1, Square::A2, Option::None),
+                    best_move: None,
                     position_evaluation: 0
                 } 
                 }
             }
-        }
-        else {
+        } else {    
             match status {
                 BoardStatus::Ongoing => return ReturnValue  { 
-                    best_move: ChessMove::new(Square::A1, Square::A2, Option::None),
-                    position_evaluation: calculate_position(board)
+                    best_move: None,
+                    position_evaluation: -calculate_position(board)
                 },
                 BoardStatus::Checkmate => return ReturnValue  { 
-                    best_move: ChessMove::new(Square::A1, Square::A2, Option::None),
-                    position_evaluation: 999999
+                    best_move: None,
+                    position_evaluation: if player {-999999} else {999999}
                 },
                 BoardStatus::Stalemate => return ReturnValue {
-                    best_move: ChessMove::new(Square::A1, Square::A2, Option::None),
+                    best_move: None,
                     position_evaluation: 0
-                } // Stalemate
+                }
             }
         }
     }
-    let mut current_best_move = ChessMove::new(Square::A1, Square::H1, Option::None);
+    let mut current_best_move = Some(ChessMove::new(Square::A1, Square::H8, None));
     let movegen =  MoveGen::new_legal(&board);
     if player {
         let mut value = -999999;
@@ -110,31 +109,29 @@ fn calculate_move(mut board: &Board, depth: u8, original_depth: u8, player: bool
         
         for legal_move in movegen {
 
-            let original_board = board.clone();
-            
             // Make move
-            board.make_move(legal_move, &mut board);
+            let new_board = board.make_move_new(legal_move);
 
             // Calculate value of line
-            let node_value = calculate_move(&mut board, depth - 1, original_depth, true, alpha, beta);
+            let node_value = calculate_move(&new_board, depth - 1, original_depth, false, optimizing_color, alpha, beta);
             
-            value = cmp::max(value, node_value.position_evaluation);
+            value = cmp::max(value, node_value.position_evaluation);    
 
-            
+
             // Prune or not to prune
             if value >= beta {
                 break;
             }
 
+            if value > best_value_move {
+                best_value_move = value;
+                current_best_move = Some(legal_move);
+            }
             alpha = cmp::max(alpha, value);
 
-            if value >= best_value_move {
-                best_value_move = value;
-                current_best_move = legal_move;
-            }
-
-            // Unmake move
-            board = &original_board;
+        }
+        if depth == original_depth {
+            println!("Black: {}", best_value_move);
         }
         return ReturnValue {
             best_move: current_best_move,
@@ -146,29 +143,29 @@ fn calculate_move(mut board: &Board, depth: u8, original_depth: u8, player: bool
 
         for legal_move in movegen {
             
-            let original_board = board.clone();
-
             // Make move
-            board.make_move(legal_move, &mut board);
+            let new_board = board.make_move_new(legal_move);
 
             // Calculate value of line
-            let node_value = calculate_move(&mut board, depth - 1, original_depth, true, alpha, beta);
+            let node_value = calculate_move(&new_board, depth - 1, original_depth, true, optimizing_color, alpha, beta);
             
             value = cmp::min(value, node_value.position_evaluation);
             
+
             // Prune or not to prune
             if value <= alpha {
                 break;
             }
-            beta = cmp::min(beta, value);
 
-            if value <= best_value_move {
+            if value < best_value_move {
                 best_value_move = value;
-                current_best_move = legal_move;
+                current_best_move = Some(legal_move);
             }
 
-            // Unmake move
-            board = &original_board;
+            beta = cmp::min(beta, value);
+        }
+        if depth == original_depth {
+            println!("White: {}", best_value_move);
         }
         return ReturnValue {
             best_move: current_best_move,
@@ -182,17 +179,21 @@ fn main() {
 
     let mut board = Board::default();
 
-    println!("{}", calculate_position(&board));
+    let mut player = false;
+    let mut optimizing_color = Color::White;
 
-    let mut player = true;
-    // for i in 1..10 {
-    //     let calculated_move = calculate_move(&board, 4, 4, player, -999999, 999999);
+    for i in 1..10 {
+        let calculated_move = calculate_move(&board, 4, 4, player, optimizing_color, -999999, 999999);
 
-    //     board = board.make_move_new(calculated_move.best_move);
-
-    //     println!("{}{}", calculated_move.best_move.get_source(), calculated_move.best_move.get_dest());
-
-    //     player = !player;
-    // }
+        match calculated_move.best_move {
+            Some(i) => {
+                board = board.make_move_new(i);
+                println!("{}{}", i.get_source(), i.get_dest());
+            },
+            None => {println!("error"); break;} 
+        }
+        player = !player;
+        if optimizing_color == Color::White {optimizing_color = Color::Black;} else {optimizing_color = Color::White;}
+    }
 
 }
