a8744998ff25807549d3e7df0502ac1e00be937d
cbacary <cbacary4@gmail.com>
2022-07-23 00:25:48 -0400

    Fix chess engine.

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,137 @@
-use chess::{Board, MoveGen, ChessMove, Square, Color, Piece, BoardStatus};
+use chess::{Board, MoveGen, ChessMove, Square, Color, Piece, BoardStatus, BitBoard};
+use gtk::gdk::keys::constants::R;
 use std::cmp;
+
+static PAWN_VALUES: &'static [f64] =    &[0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,
+                                        5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,
+                                        1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0,
+                                        0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5,
+                                        0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0,
+                                        0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5,
+                                        0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5,
+                                        0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0];
+static KNIGHT_VALUES: &'static [f64] =  &[-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0,
+                                        -4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0,
+                                        -3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0,
+                                        -3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0,
+                                        -3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0,
+                                        -3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0,
+                                        -4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0,
+                                        -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0];
+static BISHOP_VALUES: &'static [f64] =  &[-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0,
+                                        -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0,
+                                        -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0,
+                                        -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0,
+                                        -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0,
+                                        -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0,
+                                        -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0,
+                                        -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0];
+static ROOK_VALUES: &'static [f64] =    &[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,  0.0,
+                                        0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,  0.5,
+                                        -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5,
+                                        -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5,
+                                        -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5,
+                                        -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5,
+                                        -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5,
+                                        0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0,  0.0];
+static QUEEN_VALUES: &'static [f64] =   &[-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0,
+                                        -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0,
+                                        -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0,
+                                        -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5,
+                                        -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5,
+                                        -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0,
+                                        -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0,
+                                        -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0];
+static KING_VALUES: &'static [f64] =    &[-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0,
+                                        -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0,
+                                        -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0,
+                                        -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0,
+                                        -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0,
+                                        -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0,
+                                        2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0,
+                                        2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0];
+                      
+                                        
+static mut count: i32 = 0;
 
 struct ReturnValue {
     best_move: Option<ChessMove>,
-    position_evaluation: i32
+    position_evaluation: f64
 }
 
-fn calculate_position(board: &Board) -> i32 {
+fn calculate_position(board: &Board) -> f64 {
 
-    let mut position_value = 0;
+    let mut position_value = 0.0;
 
     let white_pieces = board.color_combined(Color::White);
     let black_pieces = board.color_combined(Color::Black);
 
-    let pawns = board.pieces(Piece::Pawn);
-    let rooks = board.pieces(Piece::Rook);
-    let knights = board.pieces(Piece::Knight);
-    let bishops = board.pieces(Piece::Bishop);
-    let kings = board.pieces(Piece::King);
-    let queens = board.pieces(Piece::King);
+    for x in *white_pieces {
+        let square_index = x.to_int() as usize;
+        let piece = board.piece_on(x);
+        match piece {
+            Some(Piece::Pawn) => {
+                position_value += 10.0 + PAWN_VALUES[square_index];
+            },
+            Some(Piece::Knight) => {
+                position_value += 35.0 + KNIGHT_VALUES[square_index];
+            },
+            Some(Piece::Bishop) => {
+                position_value += 37.5 + BISHOP_VALUES[square_index];
+            },
+            Some(Piece::Rook)  => {
+                position_value += 52.5 + ROOK_VALUES[square_index];
+            },
+            Some(Piece::Queen) => {
+                position_value += 100.0 + QUEEN_VALUES[square_index];
+            },
+            Some(Piece::King) => {
+                position_value += 1000.0 + KING_VALUES[square_index];
+            },
+            _ => ()
+        }
+    }
+    for x in *black_pieces {
+        let square_index = x.to_int() as usize;
+        
+        let a = (square_index / 8) + 1;
+        let b = 8 - a;
+        let c = 8 * b;
+        let d = square_index % 8;
 
-    // Calculate for white pieces
-    for _ in white_pieces & pawns {
-        position_value += 1;
-    }
-    for _ in white_pieces & rooks {
-        position_value += 5;
-    }
-    for _ in white_pieces & knights {
-        position_value += 3;
-    }
-    for _ in white_pieces & bishops {
-        position_value += 3;
-    }
-    for _ in white_pieces & kings {
-        position_value += 10000;
-    }
-    for _ in white_pieces & queens {
-        position_value += 10;
-    }
+        let square_index = c + d;
 
-    for _ in black_pieces & pawns {
-        position_value -= 1;
+        let piece = board.piece_on(x);
+        match piece {
+            Some(Piece::Pawn) => {
+                position_value -= 10.0 + PAWN_VALUES[square_index];
+            },
+            Some(Piece::Knight) => {
+                position_value -= 35.0 + KNIGHT_VALUES[square_index];
+            },
+            Some(Piece::Bishop) => {
+                position_value -= 37.5 + BISHOP_VALUES[square_index];
+            },
+            Some(Piece::Rook)  => {
+                position_value -= 52.5 + ROOK_VALUES[square_index];
+            },
+            Some(Piece::Queen) => {
+                position_value -= 100.0 + QUEEN_VALUES[square_index];
+            },
+            Some(Piece::King) => {
+                position_value -= 1000.0 + KING_VALUES[square_index];
+            },
+            _ => ()
+        }
     }
-    for _ in black_pieces & rooks {
-        position_value -= 5;
-    }
-    for _ in black_pieces & knights {
-        position_value -= 3;
-    }
-    for _ in black_pieces & bishops {
-        position_value -= 3;
-    }
-    for _ in black_pieces & kings {
-        position_value -= 10000;
-    }
-    for _ in black_pieces & queens {
-        position_value -= 10;
-    }
-
     return position_value;
-
 }
 
 // In some cases we need to return the value of the node
 
-fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, optimizing_color: Color, mut alpha: i32, mut beta: i32) -> ReturnValue {
+fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, optimizing_color: Color, mut alpha: f64, mut beta: f64) -> ReturnValue {
     let status = board.status();
+    unsafe {count += 1;}
     if depth == 0 || status != BoardStatus::Ongoing {
         if optimizing_color == Color::White {
             match status {
@@ -76,27 +141,26 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
                 },
                 BoardStatus::Checkmate => return ReturnValue  { 
                     best_move: None,
-                    position_evaluation: if player {999999} else {-999999}
+                    position_evaluation: if player {999999.0} else {-999999.0}
                 },
-                BoardStatus::Stalemate => { return ReturnValue {
+                BoardStatus::Stalemate => return ReturnValue {
                     best_move: None,
-                    position_evaluation: 0
-                } 
+                    position_evaluation: 0.0
                 }
             }
         } else {    
             match status {
                 BoardStatus::Ongoing => return ReturnValue  { 
                     best_move: None,
-                    position_evaluation: -calculate_position(board)
+                    position_evaluation: calculate_position(board)
                 },
                 BoardStatus::Checkmate => return ReturnValue  { 
                     best_move: None,
-                    position_evaluation: if player {-999999} else {999999}
+                    position_evaluation: if player {-999999.0} else {999999.0}
                 },
                 BoardStatus::Stalemate => return ReturnValue {
                     best_move: None,
-                    position_evaluation: 0
+                    position_evaluation: 0.0
                 }
             }
         }
@@ -104,8 +168,8 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
     let mut current_best_move = Some(ChessMove::new(Square::A1, Square::H8, None));
     let movegen =  MoveGen::new_legal(&board);
     if player {
-        let mut value = -999999;
-        let mut best_value_move = -999999;
+        let mut value = -999999.0;
+        let mut best_value_move = -999999.0;
         
         for legal_move in movegen {
 
@@ -115,8 +179,9 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
             // Calculate value of line
             let node_value = calculate_move(&new_board, depth - 1, original_depth, false, optimizing_color, alpha, beta);
             
-            value = cmp::max(value, node_value.position_evaluation);    
+            value = f64::max(value, node_value.position_evaluation);    
 
+            alpha = f64::max(alpha, value);
 
             // Prune or not to prune
             if value >= beta {
@@ -127,7 +192,6 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
                 best_value_move = value;
                 current_best_move = Some(legal_move);
             }
-            alpha = cmp::max(alpha, value);
 
         }
         if depth == original_depth {
@@ -138,8 +202,8 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
             position_evaluation: value
         }
     } else {
-        let mut value = 999999;
-        let mut best_value_move = 999999;
+        let mut value = 999999.0;
+        let mut best_value_move = 999999.0;
 
         for legal_move in movegen {
             
@@ -149,8 +213,9 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
             // Calculate value of line
             let node_value = calculate_move(&new_board, depth - 1, original_depth, true, optimizing_color, alpha, beta);
             
-            value = cmp::min(value, node_value.position_evaluation);
+            value = f64::min(value, node_value.position_evaluation);
             
+            beta = f64::min(beta, value);
 
             // Prune or not to prune
             if value <= alpha {
@@ -162,7 +227,7 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
                 current_best_move = Some(legal_move);
             }
 
-            beta = cmp::min(beta, value);
+
         }
         if depth == original_depth {
             println!("White: {}", best_value_move);
@@ -175,15 +240,16 @@ fn calculate_move(board: &Board, depth: u8, original_depth: u8, player: bool, op
 }
 
 fn main() {
-    
 
     let mut board = Board::default();
 
-    let mut player = false;
+    println!("peft 4: {}", MoveGen::movegen_perft_test(&board, 4));
+
+    let mut player = true;
     let mut optimizing_color = Color::White;
 
     for i in 1..10 {
-        let calculated_move = calculate_move(&board, 4, 4, player, optimizing_color, -999999, 999999);
+        let calculated_move = calculate_move(&board, 4, 4, player, optimizing_color, -999999.0, 999999.0);
 
         match calculated_move.best_move {
             Some(i) => {
@@ -194,6 +260,9 @@ fn main() {
         }
         player = !player;
         if optimizing_color == Color::White {optimizing_color = Color::Black;} else {optimizing_color = Color::White;}
+        unsafe {println!("{}", count); count = 0;}
     }
+
+
 
 }
